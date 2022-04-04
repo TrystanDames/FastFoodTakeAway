be18987acc58bca7a17968159622a067bb8ce105
Trystan <trystandames08@gmail.com>
2022-04-04 10:23:19 +0200

    1st Commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static String fullName;
+    public static int contactNumber;
+    public static double price;
+    public static double fullPrice = 0;
+    
+    //Menu
+
+    //Food
+    public static double Burger = 60.50;
+    public static double Pasta = 50.00;
+    public static double Taco = 75.99;
+
+    //Drinks
+    public static double CokeCan = 10.99;
+    public static double BotCoke = 14.99;
+    public static double Pepsi = 12.99;
+    public static double Water = 11.99;
+    public static double Monster = 20.00;
+
+    //Sides
+    public static double Chips = 14.00;
+    public static double Salad = 10.50;
+    public static double OnionRings = 15.00;
+    public static double Sauce = 12.00;
+
+    static MainClass mc;
+
+    public static void main(String[] args){
+        
+    }
+}
---
+++ MainClass.java
@@ -0,0 +1,30 @@
+public class MainClass {
+
+    private String fullName;
+    private int contactNumber;
+    private double fullPrice;
+
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+
+    public String getFullName(){
+        return fullName;
+    }
+
+    public void setcontactNumber(int contactNumber){
+        this.contactNumber = contactNumber;
+    }
+
+    public int getcontactNumber(){
+        return contactNumber;
+    }
+
+    public void setfullPrice(double fullPrice){
+        this.fullPrice = fullPrice;
+    }
+
+    public double getfullPrice(){
+        return fullPrice;
+    }
+}
